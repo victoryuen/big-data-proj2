81d0004814bc91c9eafb33630567f7737db84ddd
Victor Yuen <dhuce3@gmail.com>
2024-05-01 18:51:30 -0400

    mapper + reduce function

--- main.py
+++ main.py
@@ -9,17 +9,13 @@ def create_dict(dataframe):
 def populate_dict(dataframe,dictionary):
     for index,row in dataframe.iterrows():
         dictionary[row["source"]].append(row["target"])
-#mapper function
-        
-#notes:
-
-# def temp(dictionary):
-#     for k,v in dictionary.iterrows():
-# def mapper(dictionary):
-#     print(dictionary)
-#     # person, friends = dictionary[0]
-#     # # print(person)
-#     # return [(person, 1) for _ in friends]
+def mapper(compound_binds):
+    compound, binds = compound_binds
+    return [(compound, 1) for _ in binds]
+def reducer(counts, pair):
+    compound, bind_count = pair
+    counts[compound] = counts.get(compound, 0) + bind_count
+    return counts
 
 def main():
     nodes_df = pd.read_csv('nodes_test.tsv', sep='\t')
@@ -28,7 +24,12 @@ def main():
     dict_compounds = create_dict(compound_nodes_df)
     binds_df = edges_df[edges_df.metaedge == "CbG"]
     populate_dict(binds_df,dict_compounds)
-    mapped_data = reduce(lambda x, y: x + y, map(mapper, dict_compounds))
+    #map data
+    mapped_data = reduce(lambda x, y: x + y, map(mapper, dict_compounds.items()))
+    #reduce 
+    compound_counts = reduce(reducer, mapped_data, {})
+    print(compound_counts.get("Compound::DB01268"))
+    print(max(compound_counts, key=compound_counts.get))
     
 if __name__ == "__main__":
     main()
