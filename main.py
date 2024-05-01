ac8cc358e1a7928366d4e28c40d880ad34b18334
Victor Yuen <dhuce3@gmail.com>
2024-04-30 20:10:31 -0400

    remove classes added .gitignore changing df implementation classes -> dicts

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ def main():
     edges_df = pd.read_csv('edges_test.tsv', sep='\t')
     
     list_of_compounds = []
+    dict_compounds = {}
     compound_nodes_df = nodes_df[nodes_df.kind == "Compound"]
     binds_df = edges_df[edges_df.metaedge == "CbG"]
-    print(len(binds_df[binds_df.source == "Compound::DB01435"]))
+    #print(len(binds_df[binds_df.source == "Compound::DB01435"]))
     for index,row in compound_nodes_df.iterrows():
-        compound = Compound(row["id"],row["name"])
-        list_of_compounds.append(compound)
+        dict_compounds[row["name"]] = []
+        
     
 if __name__ == "__main__":
     main()
