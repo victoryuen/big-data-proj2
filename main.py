8cd3e69d98baa728156e51467bc28e2c341d973c
victoryuen <dhuce3@gmail.com>
2024-05-01 00:28:24 -0400

    implemented a function to populate the dictionary with the edges for compounds

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import pandas as pd
 def create_dict(dataframe):
     dictionary = {}
     for index,row in dataframe.iterrows():
-        dictionary[row["name"]] = []
+        dictionary[row["id"]] = []
+    return dictionary
 def populate_dict(dataframe,dictionary):
-    dictionary["MagicpillA"] += "1"
+    for index,row in dataframe.iterrows():
+        dictionary[row["source"]].append(row["target"])
 
 def main():
-
+    checker = input("Enter a compound:")
     nodes_df = pd.read_csv('nodes_test.tsv', sep='\t')
     edges_df = pd.read_csv('edges_test.tsv', sep='\t')
     compound_nodes_df = nodes_df[nodes_df.kind == "Compound"]
     dict_compounds = create_dict(compound_nodes_df)
-    print(dict_compounds)
     binds_df = edges_df[edges_df.metaedge == "CbG"]
     populate_dict(binds_df,dict_compounds)
-   
     
     
 if __name__ == "__main__":
