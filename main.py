95477f154656cfb556a8d653674816663f63970f
Victor Yuen <dhuce3@gmail.com>
2024-04-30 23:33:29 -0400

    adding functions making it more clean

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import pandas as pd 
 
-
+def create_dict(dataframe):
+    dictionary = {}
+    for index,row in dataframe.iterrows():
+        dictionary[row["name"]] = []
+def populate_dict(dataframe,dictionary):
+    dictionary["MagicpillA"] += "1"
 
 def main():
 
     nodes_df = pd.read_csv('nodes_test.tsv', sep='\t')
     edges_df = pd.read_csv('edges_test.tsv', sep='\t')
-    
-    list_of_compounds = []
-    dict_compounds = {}
     compound_nodes_df = nodes_df[nodes_df.kind == "Compound"]
+    dict_compounds = create_dict(compound_nodes_df)
+    print(dict_compounds)
     binds_df = edges_df[edges_df.metaedge == "CbG"]
-    #print(len(binds_df[binds_df.source == "Compound::DB01435"]))
-    for index,row in compound_nodes_df.iterrows():
-        dict_compounds[row["name"]] = []
-        
+    populate_dict(binds_df,dict_compounds)
+   
+    
+    
 if __name__ == "__main__":
     main()
