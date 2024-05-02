dd3ede192a20ef00fdadf7014d310d58f1da6008
victoryuen <dhuce3@gmail.com>
2024-05-02 00:05:14 -0400

    adding more functions nodes and edges => creating and populating dicts mapper + reducer ( specific for compounds so far havent seen if future questions also need, if so i can change) + top 5 compounds

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd 
 from functools import reduce
 
+nodes_df = pd.read_csv('nodes_test.tsv', sep='\t')
+edges_df = pd.read_csv('edges_test.tsv', sep='\t')
 def create_dict(dataframe):
     dictionary = {}
     for index,row in dataframe.iterrows():
@@ -16,21 +18,31 @@ def reducer(counts, pair):
     compound, bind_count = pair
     counts[compound] = counts.get(compound, 0) + bind_count
     return counts
+def get_relationships(node_type, edge_type):
+    """
+    Given a source node type and and edge type, 
+    returns a dictionary with all the source nodes
+    mapped to their corresponding target node
+    via the specific edge type.
+    """
+    
+    source_nodes_df = nodes_df[nodes_df.kind == node_type]
+    dict_nodes = create_dict(source_nodes_df)
+    relationships_df = edges_df[edges_df.metaedge == edge_type]
+    populate_dict(relationships_df, dict_nodes)
+    return dict_nodes
 
 def main():
-    nodes_df = pd.read_csv('nodes_test.tsv', sep='\t')
-    edges_df = pd.read_csv('edges_test.tsv', sep='\t')
-    compound_nodes_df = nodes_df[nodes_df.kind == "Compound"]
-    dict_compounds = create_dict(compound_nodes_df)
-    binds_df = edges_df[edges_df.metaedge == "CbG"]
-    populate_dict(binds_df,dict_compounds)
-    #map data
+    dict_compounds = get_relationships("Compound","CbG")
+    # map data
     mapped_data = reduce(lambda x, y: x + y, map(mapper, dict_compounds.items()))
-    #reduce 
+    # reduce 
     compound_counts = reduce(reducer, mapped_data, {})
     print(compound_counts.get("Compound::DB01268"))
-    print(max(compound_counts, key=compound_counts.get))
-    
+    #Top 5 
+    #print(compound_counts.items())
+    top_compounds = sorted(compound_counts.items(), key= lambda x: x[1],reverse=True)
+    print("The top 5 compounds are ", top_compounds[:5])
 if __name__ == "__main__":
     main()
 
